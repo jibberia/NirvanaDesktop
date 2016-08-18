53a9533362f019ca7b484bb59dbeb6541341431a
Kevin Cox <kevinc@candicontrols.com>
2016-08-18 07:51:07 -0700

    Initial Commit

---
+++ NirvanaDesktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NirvanaDesktop
+//
+//  Created by Kevin Cox on 8/18/16.
+//  Copyright © 2016 Kevin Cox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
